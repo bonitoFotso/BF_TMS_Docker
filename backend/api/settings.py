f1ad61bcef05541943caae4d230358ea43f8a3cc
bonitoFotso <bonitofotso55@gmai.com>
2023-11-17 17:30:40 +0100

    123

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -158,9 +158,9 @@ DATABASES = {
     "default": {
         "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
         "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
-        "USER": os.environ.get("SQL_USER", "user"),
-        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
-        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "USER": os.environ.get("SQL_USER", "api_dev_user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "api_dev_pword"),
+        "HOST": os.environ.get("SQL_HOST", "db"),
         "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
