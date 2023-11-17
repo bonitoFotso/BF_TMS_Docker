ed84768832a07763cdd34ce02a4e9f44da504c85
bonitoFotso <bonitofotso55@gmai.com>
2023-11-17 08:04:59 +0100

    good 1.0.2

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -33,11 +33,14 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
     "http://127.0.0.1:8080",
     ##"*",
-    "http://192.168.44.47:8080",  # Ajoutez l'adresse de votre réseau local
-    "http://192.168.44.47:3000",
+    #"http://192.168.44.47:8080",  # Ajoutez l'adresse de votre réseau local
+    #"http://192.168.44.47:3000",
     "http://192.168.44.41:8000",
     "http://192.168.44.41:3000",
     "http://192.168.44.41:8080",
+    "http://23.227.178.54:8000",
+    "http://23.227.178.54:3000",
+    "http://23.227.178.54:8080",
    #"http://192.168.44.0/24",  # Ou utilisez le masque CIDR pour le réseau local
    #"http://*.*.*.*",  # Utilisez l'astérisque pour autoriser toutes les origines
    #"https://*.*.*.*",
